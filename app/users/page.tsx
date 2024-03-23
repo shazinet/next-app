06b7bf0533daabf20e82639f9f0099ca6f2251ac
Hoomaan <id@hoomaan.dev>
2024-03-23 16:57:25 +0400

    Fetch data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users List</h1>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </>
+  );
 };
 
 export default UsersPage;
