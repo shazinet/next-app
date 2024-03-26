88e49dc7f44bef72780ce7daab360fcd0259a2fa
Hoomaan <id@hoomaan.dev>
2024-03-26 12:00:44 +0400

    validate requests with Zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,5 @@
-import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 interface Props {
   params: { id: number };
@@ -19,8 +19,9 @@ export async function PUT(
   { params }: { params: { id: number } }
 ) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   if (params.id > 10)
     return NextResponse.json({ error: "User does not exist" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
