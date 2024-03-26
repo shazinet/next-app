71427300c4e1efdbf8d0d2c176f43ac83af179b0
Hoomaan <id@hoomaan.dev>
2024-03-26 11:31:57 +0400

    Create an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
@@ -5,4 +6,11 @@ export function GET(request: NextRequest) {
     { id: 1, name: "Hoomaan" },
     { id: 2, name: "Monarch" },
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
