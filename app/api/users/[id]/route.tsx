32407beebee49a5b1f86848778a448d75166dc78
Hoomaan <id@hoomaan.dev>
2024-03-26 11:46:14 +0400

    Delete an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -25,3 +25,12 @@ export async function PUT(
     return NextResponse.json({ error: "User does not exist" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User does not exist" }, { status: 404 });
+  return NextResponse.json({});
+}
