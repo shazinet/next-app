397ec7fc514fad7402ac6798ee2265195c80a36a
Hoomaan <id@hoomaan.dev>
2024-03-26 11:42:33 +0400

    Update an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
@@ -11,4 +12,16 @@ export function GET(
   if (params.id > 10)
     return NextResponse.json({ error: "User doesn't exist" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Hoomaan" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  if (params.id > 10)
+    return NextResponse.json({ error: "User does not exist" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
 }
