621bf08b18d5409b3fbac3d8fa3b80117237cd01
Hoomaan <id@hoomaan.dev>
2024-03-27 20:09:20 +0400

    Prisma - get data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,17 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
 interface Props {
   params: { id: number };
 }
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user)
     return NextResponse.json({ error: "User doesn't exist" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: "Hoomaan" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Hoomaan" },
-    { id: 2, name: "Monarch" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
