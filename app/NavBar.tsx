a71780696f99af4a5916af1905234e80b3c900b8
Hoomaan <id@hoomaan.dev>
2024-03-30 21:13:51 +0400

    Sign out users

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,7 +14,14 @@ const NavBar = () => {
       </Link>
       <Link href="/users">Users</Link>
       {status === "loading" && <div>Loading...</div>}
-      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "authenticated" && (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Sign out
+          </Link>
+        </div>
+      )}
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Login</Link>
       )}
