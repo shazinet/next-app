fa63bd49abf256d3dc12f915ab786cd74159de75
Hoomaan <id@hoomaan.dev>
2024-03-31 17:39:30 +0400

    Create an email template

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome Aboard!</Preview>
+      <Body>
+        <Container>
+          <Text>Hello {name}</Text>
+          <Link href="https://eden.bz">www.eden.bz</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
