58aeb3b13d9e3ee359ed7ab85f83fb2a666e03e9
Hoomaan <id@hoomaan.dev>
2024-03-31 22:57:41 +0400

    Lazy load - Dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import { Metadata } from "next";
+"use client";
 
-export default async function Home() {
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false,
+  loading: () => <div>Loading...</div>,
+});
+
+export default function Home() {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <main className="relative h-screen">
       <h1>Hello World!</h1>
+      <button className="btn btn-secondary" onClick={() => setVisible(true)}>
+        Show component
+      </button>
+      {isVisible && <HeavyComponent />}
     </main>
   );
 }
