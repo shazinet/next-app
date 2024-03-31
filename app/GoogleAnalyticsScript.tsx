f5d0c3a6e7ab8524bd2920c9120677883a239b8e
Hoomaan <id@hoomaan.dev>
2024-03-31 21:50:59 +0400

    Use third party scripts

---
+++ app/GoogleAnalyticsScript.tsx
@@ -0,0 +1,22 @@
+import Script from "next/script";
+import React from "react";
+
+const GoogleAnalyticsScript = () => {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-LRSVSY1205"
+      />
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LRSVSY1205');`}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalyticsScript;
