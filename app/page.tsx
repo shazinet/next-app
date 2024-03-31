cb72e8784a6f6ff2a3b8e23025fe3229189d508a
Hoomaan <id@hoomaan.dev>
2024-03-31 21:37:46 +0400

    Optimize images

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import fatemeh from "@/public/images/fatemeh.PNG";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}!</h1>
-      <Link href="/users">Users Page</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="fatemeh"
+        // width={300}
+        // height={170}
+        fill
+        // style={{ objectFit: "cover" }} //cover, contain
+        className="object-cover" // Tailwind
+        sizes="(max-width: 480px) 100vw, (max-width: 768) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
